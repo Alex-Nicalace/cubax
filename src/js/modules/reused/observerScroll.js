ed05f6523124c471bfd58f2cf80ae1e259abc2cf
Alex-Nicalace <anicalace@gmail.com>
2023-08-13 16:16:01 +0300

    change ciolor in header

--- src/js/modules/reused/observerScroll.js
+++ src/js/modules/reused/observerScroll.js
@@ -1,57 +1,110 @@
 export function createIntersectionObserver(targetSelector, cb, options = {}) {
-   const observer = new IntersectionObserver(cb, {
-      root: null, // null - пересечение с областью видимости, либо указывается родительский элемент
-      rootMargin: '0px',
-      threshold: 1, // степень пересечения объекта при которой срваботает колбэк. 1 - 100%
+  const observer = new IntersectionObserver(cb, {
+    root: null, // null - пересечение с областью видимости, либо указывается родительский элемент
+    rootMargin: '0px',
+    threshold: 1, // степень пересечения объекта при которой срваботает колбэк. 1 - 100%
 
-      /* обязательные параметры для того чтобы работало свойство entry.isVisible - Свойство isVisible является 
+    /* обязательные параметры для того чтобы работало свойство entry.isVisible - Свойство isVisible является 
       частью предлагаемых обновлений Intersection Observer v2, касающихся фактической видимости целевого 
       элемента для пользователя.*/
-      trackVisibility: true,
-      delay: 100 // minimum 100
-      , ...options
-   });
+    trackVisibility: true,
+    delay: 100, // minimum 100
+    ...options,
+  });
 
-   const targets = document.querySelectorAll(targetSelector);
-   for (const target of targets) {
-      observer.observe(target);
-   }
+  const targets = document.querySelectorAll(targetSelector);
+  for (const target of targets) {
+    observer.observe(target);
+  }
 }
 
-// new CreateIntersectionObserver('selectorOrNode', cb, {root: selectorOrNode})
-export class CreateIntersectionObserver {
-   constructor(selectorOrElement, cb, options = {}) {
-      const { root: prevRoot } = options;
-      this.observer = new IntersectionObserver(cb, {
-         root: null, // null - пересечение с областью видимости, либо указывается родительский элемент
-         rootMargin: '0px',
-         threshold: 1, // степень пересечения объекта при которой срваботает колбэк. 1 - 100%
+/**
+ * Функция обработски события пересечения
+ * @param {IntersectionObserverEntry[]} entries
+ */
+// const cb = (entries) => {
+//   console.log(entries);
+//   entries.forEach((entry) => {
+//   });
+// };
 
-         /* обязательные параметры для того чтобы работало свойство entry.isVisible - Свойство isVisible является 
-         частью предлагаемых обновлений Intersection Observer v2, касающихся фактической видимости целевого 
-         элемента для пользователя.*/
-         trackVisibility: true,
-         delay: 100 // minimum 100
-         , ...options
-         , root: typeof (prevRoot) === 'string' ? document.querySelector(prevRoot) : prevRoot,
-      });
-      this.targets = [];
-      if (typeof (selectorOrElement) === 'string') {
-         this.targets = [...document.querySelectorAll(selectorOrElement)];
-      } else if (selectorOrElement.tagName) {
-         this.targets.push(selectorOrElement);
-      };
-   }
-   observe() {
-      this.targets.forEach(target => {
-         this.observer.observe(target);
-      });
-   }
-   unobserve() {
-      this.targets.forEach(target => {
-         this.observer.unobserve(target);
-      });
-   }
+/**
+ * Класс для создания Intersection Observer с дополнительными функциональностями.
+ */
+export class CreateIntersectionObserver {
+  /**
+   * Колбэк-функция, вызываемая при пересечении наблюдаемого элемента.
+   *
+   * @callback IntersectionObserverCallback
+   * @param {Array<IntersectionObserverEntry>} entries - Массив объектов Intersection Observer Entry, представляющих состояние пересечения наблюдаемых элементов.
+   * @param {IntersectionObserver} observer - Объект Intersection Observer, используемый для наблюдения за элементами.
+   * @returns {void}
+   */
+
+  /**
+   * Создает новый экземпляр Intersection Observer.
+   * @param {string|Element} selectorOrElement - Селектор CSS или элемент, для которого будет создан Intersection Observer.
+   * @param {IntersectionObserverCallback} cb - Колбэк-функция, вызываемая при пересечении наблюдаемого элемента.
+   * @param {IntersectionObserverInit} [options={}] - Настройки Intersection Observer.
+   */
+  constructor(selectorOrElement, cb, options = {}) {
+    /**
+     * @type {IntersectionObserver}
+     * Объект Intersection Observer для наблюдения за элементами.
+     */
+    this.observer = new IntersectionObserver(cb, {
+      root: null, // null - пересечение с областью видимости, либо указывается родительский элемент
+      rootMargin: '0px', // положительное значение расширяет рамки изначального размера root, отрицательные уменьшает во внутрь
+      threshold: 0, // степень пересечения объекта при которой сработает колбэк. 1 - 100%
+      /**
+       * Обязательные параметры для использования свойства `entry.isVisible`.
+       * @type {boolean}
+       */
+      trackVisibility: true,
+      /**
+       * Задержка перед вызовом колбэка после пересечения.
+       * @type {number}
+       */
+      delay: 100, // minimum 100
+      ...options,
+      root:
+        typeof options.root === 'string'
+          ? document.querySelector(options.root)
+          : options.root,
+    });
+
+    /**
+     * @type {Array<Element>}
+     * Массив целевых элементов для наблюдения.
+     */
+    this.targets = [];
+
+    if (typeof selectorOrElement === 'string') {
+      // Если передан селектор, находим все соответствующие элементы и добавляем их в массив targets.
+      this.targets = [...document.querySelectorAll(selectorOrElement)];
+    } else if (selectorOrElement.tagName) {
+      // Если передан элемент, добавляем его в массив targets.
+      this.targets.push(selectorOrElement);
+    }
+  }
+
+  /**
+   * Запускает наблюдение за целевыми элементами.
+   */
+  observe() {
+    this.targets.forEach((target) => {
+      this.observer.observe(target);
+    });
+  }
+
+  /**
+   * Прекращает наблюдение за целевыми элементами.
+   */
+  unobserve() {
+    this.targets.forEach((target) => {
+      this.observer.unobserve(target);
+    });
+  }
 }
 
 // Example callback
--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,6 +1,7 @@
 import ScrollWindow from './reused/scrollWindow.js';
 import { mask } from './reused/mask-input-tel-new.js';
 import scrollToAnchor from './reused/scrollToAnchor.js';
+import { CreateIntersectionObserver } from './reused/observerScroll.js';
 
 const scrollWindow = new ScrollWindow();
 scrollWindow.toggleClassesHeader();
@@ -8,3 +9,30 @@ scrollWindow.toggleClassesHeader();
 mask('.input-block__input[type="tel"]', '+38 (___) ___-__-__');
 
 scrollToAnchor();
+
+/**
+ *
+ * @param {IntersectionObserverEntry[]} entries
+ */
+const onIntersection = (entries) => {
+  console.log(entries);
+  const headerEl = document.querySelector('header.header');
+  entries.forEach((entry) => {
+    const isDark =
+      entry.boundingClientRect.top < 0 && entry.boundingClientRect.bottom > 0;
+    if (isDark) {
+      headerEl.classList.add('header_sombre');
+    } else {
+      headerEl.classList.remove('header_sombre');
+    }
+  });
+};
+const scroll = new CreateIntersectionObserver(
+  '[class*="_dark"]',
+  onIntersection,
+  {
+    threshold: [0],
+    rootMargin: '0px 0px -100% 0px',
+  }
+);
+scroll.observe();
